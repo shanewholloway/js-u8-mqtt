119ede66f439ee88c6f7d914beee12452ff4921a
Shane Holloway <shane.holloway@ieee.org>
2021-10-15 15:29:00 -0600

    v0.0.26

--- esm/web/v4.mjs
+++ esm/web/v4.mjs
@@ -392,6 +392,50 @@ function mqtt_decode_pingxxx(ns) {
   return ns[0xc] = ns[0xd] = pkt => pkt
 }
 
+function mqtt_decode_disconnect(ns) {
+  const _disconnect_reason_ = bind_reason_lookup([
+    // MQTT 5.0
+    [ 0x00, 'Normal disconnection'],
+    [ 0x04, 'Disconnect with Will Message'],
+    [ 0x80, 'Unspecified error'],
+    [ 0x81, 'Malformed Packet'],
+    [ 0x82, 'Protocol Error'],
+    [ 0x83, 'Implementation specific error'],
+    [ 0x87, 'Not authorized'],
+    [ 0x89, 'Server busy'],
+    [ 0x8B, 'Server shutting down'],
+    [ 0x8D, 'Keep Alive timeout'],
+    [ 0x8E, 'Session taken over'],
+    [ 0x8F, 'Topic Filter invalid'],
+    [ 0x90, 'Topic Name invalid'],
+    [ 0x93, 'Receive Maximum exceeded'],
+    [ 0x94, 'Topic Alias invalid'],
+    [ 0x95, 'Packet too large'],
+    [ 0x96, 'Message rate too high'],
+    [ 0x97, 'Quota exceeded'],
+    [ 0x98, 'Administrative action'],
+    [ 0x99, 'Payload format invalid'],
+    [ 0x9A, 'Retain not supported'],
+    [ 0x9B, 'QoS not supported'],
+    [ 0x9C, 'Use another server'],
+    [ 0x9D, 'Server moved'],
+    [ 0x9E, 'Shared Subscriptions not supported'],
+    [ 0x9F, 'Connection rate exceeded'],
+    [ 0xA0, 'Maximum connect time'],
+    [ 0xA1, 'Subscription Identifiers not supported'],
+    [ 0xA2, 'Wildcard Subscriptions not supported'],
+  ]);
+
+
+  return ns[0xe] = (pkt, u8_body) => {
+    if (u8_body && 5 <= pkt.mqtt_level) {
+      const rdr = new mqtt_type_reader(u8_body, 0);
+      pkt.reason = rdr.u8_reason(_disconnect_reason_);
+      pkt.props = rdr.props();
+    }
+    return pkt }
+}
+
 function mqtt_decode_auth(ns) {
   const _auth_reason_ = bind_reason_lookup([
     // MQTT 5.0
@@ -780,6 +824,7 @@ function mqtt_session_ctx(mqtt_level) {
       _bind_mqtt_session_ctx(
         [ // lst_decode_ops
           mqtt_decode_connack,
+          mqtt_decode_disconnect,
           mqtt_decode_publish,
           mqtt_decode_puback,
           mqtt_decode_pubxxx,
@@ -1215,6 +1260,7 @@ class MQTTBaseClient {
       mqtt_connack(pkt, ctx) ::
       mqtt_disconnect(pkt, ctx) ::
 
+      mqtt_publish(pkt, ctx)
       mqtt_subscribe(pkt, ctx) ::
       mqtt_unsubscribe(pkt, ctx) ::
 
@@ -1336,7 +1382,6 @@ class MQTTCoreClient extends MQTTBaseClient {
 
 
   
-
 
 
 
