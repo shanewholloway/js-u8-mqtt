99693c1433f8730e66a25ed0249bbf7b5eda9f52
Shane Holloway <shane.holloway@ieee.org>
2021-11-15 15:15:52 -0700

    Updated demo config to use docker-based mqtt servers. Apparently test.mosquitto.org isnt accepting websocket connections

--- demo/support_config.mjs
+++ demo/support_config.mjs
@@ -1,6 +1,19 @@
-export const cfg_localhost = {
-  tcp: { port: 5883, host: '127.0.0.1'},
-  websock_url: 'ws://127.0.0.1:9001',
+export const cfg_integ = {
+  // configs matching `npm run dkr_deps` -- ../test/integ/deps-deploy.yml
+  mosquitto_v16: {
+    tcp: { port: 9883, host: '127.0.0.1'},
+    websock_url: 'ws://127.0.0.1:9880',
+  },
+
+  mosquitto_v20: {
+    tcp: { port: 9893, host: '127.0.0.1'},
+    websock_url: 'ws://127.0.0.1:9890',
+  },
+
+  ejabberd: {
+    tcp: { port: 5883, host: '127.0.0.1'},
+    websock_url: 'ws://127.0.0.1:5880',
+  },
 }
 
 export const cfg_test_mosquitto_org = {
@@ -8,4 +21,5 @@ export const cfg_test_mosquitto_org = {
   websock_url: 'wss://test.mosquitto.org:8081',
 }
 
-export default cfg_test_mosquitto_org
+//export default cfg_test_mosquitto_org
+export default cfg_integ.mosquitto_v16
